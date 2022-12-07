b6f5c4062618fc1c1ba6e66eece68be97b3d6f5b
Shubham <shubhamsinghlmp578@gmail.com>
2022-12-07 19:16:20 +0530

    Lombok Dependency added and logging enabled for the project...

--- src/main/java/com/example/RestAPI_MVC/UserController.java
+++ src/main/java/com/example/RestAPI_MVC/UserController.java
@@ -1,5 +1,8 @@
 package com.example.RestAPI_MVC;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    public Logger logger  = LoggerFactory.getLogger(UserController.class);
+
+
+
     @GetMapping("/get_users")
     public ResponseEntity<List<User>> getUsers(){
+        logger.info("user printed");
         return new ResponseEntity<>(userService.getUserService() , HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/post_user")
     public ResponseEntity<String> postUser(@RequestBody(required = true)User user){
+        logger.info("User added");
         userService.addUserService(user);
         return new ResponseEntity<>("User Added to the Database",HttpStatus.CREATED);
     }
